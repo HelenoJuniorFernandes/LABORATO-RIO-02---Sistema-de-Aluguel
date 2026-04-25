6f9171944d04a6271294fb4e9d317b4405d14d98
HelenoJuniorFernandes <helenojunior1313@gmail.com>
2026-04-24 23:09:10 -0300

    fix: muda execucao do data.sql do h2 para o hibernate apos a criacao das tabelas

--- rental-system/src/main/java/com/pucminas/rental_system/controller/AuthController.java
+++ rental-system/src/main/java/com/pucminas/rental_system/controller/AuthController.java
@@ -3,6 +3,7 @@ package com.pucminas.rental_system.controller;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.http.annotation.QueryValue;
 import io.micronaut.security.annotation.Secured;
 import io.micronaut.security.rules.SecurityRule;
 import io.micronaut.views.View;
@@ -18,11 +19,16 @@ public class AuthController {
     
     @View("login")
     @Get("/login")
-    public HttpResponse<?> login(@Nullable Principal principal) {
+    public HttpResponse<?> login(@Nullable Principal principal, 
+                                 @Nullable @QueryValue Boolean error, 
+                                 @Nullable @QueryValue Boolean logout) {
         if (principal != null) {
             return HttpResponse.redirect(URI.create("/pedidos"));
         }
-        return HttpResponse.ok(Map.of());
+        return HttpResponse.ok(Map.of(
+            "error", error != null && error,
+            "logout", logout != null && logout
+        ));
     }
 
     @Get("/")
